5535201fdbdd922c88f68e4d01ff3f830a949c9a
Gal Shashua <galshashua@gmail.com>
2020-05-02 22:59:00 +0100

    try2

---
+++ Concentration_HW1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration_HW1
+//
+//  Created by user167535 on 5/1/20.
+//  Copyright © 2020 user167535. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("test")
+        print("test2")
+        print("test3")
+
+    }
+
+
+}
+
