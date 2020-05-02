fb035555e2cdff23560ae9197ab8497ff1261dd6
Gal Shashua <galshashua@gmail.com>
2020-05-02 23:00:50 +0100

    upload first commit to git

--- Concentration_HW1/ViewController.swift
+++ Concentration_HW1/ViewController.swift
@@ -14,9 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("test")
-        print("test2")
-        print("test3")
+        print("connect to  github succcess")
 
     }
 
